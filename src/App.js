ac08545a9a8a6db9743f08541d8ce8cd029aee41
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-10-08 03:07:56 -0400

    characters show in DOM and added some styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from "./components/Character"
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(resp => {
+      console.log(resp.data)
+        setCharacters(resp.data)
+    }).catch(err => {
+      console.error(err);
+    })
+  
+  }, [])
+  
+
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars</h1>
+      {
+        characters.map(elem => {
+          return <Character info={elem} />
+        })
+
+
+
+      }
+     
+     
+  
+
+      
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import App from "../App"
+import styled from "styled-components";
+import '../App.css';
+
+
+const StyledCharacters = styled.div`
+display: flex;
+justify-content: flex-start;
+color: white;;
+padding: 4%;
+
+`
+
+
+
+
+
+
+const Character = (prop) => {
+    return (
+        <div className="container">
+      <StyledCharacters style={{color: 'black'}} >
+        <button className="btn">{prop.info.name}</button>
+        </StyledCharacters>
+        </div>
+    )
+}
+
+export default Character
+
